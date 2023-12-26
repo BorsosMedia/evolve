86ad200679cfef2742a861557e9ee4080231bf91
Luis Landaeta <luis.landaeta24@gmail.com>
2023-12-26 14:06:36 -0400

    Hero adjustments

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,14 +5,14 @@ import hero1 from "../public/assets/hero-1.JPG";
 
 const Hero = () => {
   return (
-    <article className="mt-32 grid grid-cols-1 place-content-center sm:grid-cols-2">
+    <article className="container mr-auto mt-32 grid grid-cols-2 place-content-center gap-5">
       <Image
         src={hero1}
         alt="Training partners"
-        className="hidden h-auto w-80 sm:block"
+        className="block h-auto w-auto"
       />
-      <section className="flex flex-col items-center gap-4 text-center sm:items-start sm:text-left">
-        <h1 className="uppercase">
+      <section className="flex flex-col items-start gap-4 text-left">
+        <h1 className="text-2xl uppercase sm:text-5xl">
           <span>Change your mindset</span>
           <br />
           <span className="font-black text-red">Change your life</span>
@@ -22,7 +22,9 @@ const Hero = () => {
           To become your best version you need to be ready to transform
           yourself...
         </p>
-        <p className="font-black">You need to be ready to EVOLVE</p>
+        <p className="text-lg font-black sm:text-xl">
+          You need to be ready to EVOLVE
+        </p>
       </section>
     </article>
   );
